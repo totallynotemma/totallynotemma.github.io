63cc8693ff02182a342304d00613d4261d16bc31
totallynotemma <57207128+totallynotemma@users.noreply.github.com>
2021-03-21 15:49:25 +1030

    lol cool nav changes go brr

--- assets/java/main.js
+++ assets/java/main.js
@@ -1,10 +1,16 @@
-function openTab(tabName) {
+function openTab(tabName, e) {
   var tabName = document.getElementById(tabName)
   var tab = document.getElementsByClassName('tab')
+  var tabMenuItem = document.getElementsByClassName('nav_item')
+
+  for (i = 0; i < tabMenuItem.length; i++) {
+    tabMenuItem[i].classList.remove('nav_active')
+  }
 
   for (i=0;i<tab.length;i++) {
     tab[i].style.display = 'none'
   }
 
   tabName.style.display = 'block'
+  e.currentTarget.classList.add('nav_active')
 }
